8c85ef97ea9d34cef502eac826065c377e01f0c3
cleophass <95467960+cleophass@users.noreply.github.com>
2023-10-07 20:44:00 +0200

    test

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ def clean_characteristics(df):
     return df
 
 
-@st.cache_data  #
+@st.cache(allow_output_mutation=True)
 def load_data(year):
     base_path = f"assets/{year}"
     caracteristiques = pd.read_csv(
